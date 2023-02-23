b09ceb9a02318103733924dc7c12775426c81b3e
Fadly <fadly.setiawan@biomarking.com>
2023-02-23 15:05:17 +0700

    add slider component

--- src/components/slider-image.js
+++ src/components/slider-image.js
@@ -1,32 +1,48 @@
 import Slider from "react-slick";
+import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/24/solid'
+
 
 const SliderImage = () => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    arrows: false,
     slidesToShow: 1,
     dots: false,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    nextArrow: <ChevronRightIcon/>,
+    prevArrow: <ChevronLeftIcon/>
   };
 
   return (
     <Slider {...settings}>
+      <div className="flex relative">
+        <img
+          className="rounded-t-md"
+          width="544px"
+          height="272px"
+          src="https://via.placeholder.com/544x272"
+          alt="Placeholder"
+        />
+      </div>
+      <div className="flex relative">
       <img
-        className="w-full h-60 object-cover rounded-t-md"
-        width="544px"
-        height="272px"
-        src="https://via.placeholder.com/544x272"
-        alt="Placeholder"
-      />
-      <img
-      className="w-full h-60 object-cover rounded-t-md"
+      className="rounded-t-md"
       width="544px"
       height="272px"
       src="https://via.placeholder.com/544x272"
       alt="Placeholder"
     />
+      </div>
+      <div className="flex relative">
+      <img
+      className="rounded-t-md"
+      width="544px"
+      height="272px"
+      src="https://via.placeholder.com/544x272"
+      alt="Placeholder"
+    />
+      </div>
     </Slider>
   )
 }
